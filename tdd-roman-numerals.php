35a514b693a899a45d77c449c09a3fcecca21a12
Mihail Stoian <stoian.mihail.alexandru@gmail.com>
2019-05-25 10:43:40 +0300

    Roman numerals I,II wip -RED;

--- tdd-roman-numerals.php
+++ tdd-roman-numerals.php
@@ -5,3 +5,11 @@ function toRomanNumeral ($n){
 
 echo "Test 1: When passing 1 returns I";
 print_r("<br>Result:".(toRomanNumeral(1)=="I"?"true":"false"));
+echo "<br>";
+
+echo "Test 1: When passing 2 returns II";
+print_r("<br>Result:".(toRomanNumeral(2)=="II"?"true":"false"));
+echo "<br>";
+
+
+
