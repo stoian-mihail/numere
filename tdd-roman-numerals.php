3999dc00f53104875d672d4f1a517e23eb2399ba
Mihail Stoian <stoian.mihail.alexandru@gmail.com>
2019-05-25 12:04:56 +0300

    Roman numerals 1-10000 -GREEN;

--- tdd-roman-numerals.php
+++ tdd-roman-numerals.php
@@ -1,15 +1,18 @@
 <?php
 function toRomanNumeral ($n){
+    $map=array("M"=>1000,"CM"=>900,"D"=>500,"CD"=>400,"C"=>100,"XC"=>90,"L"=>50,"XL"=>40,"X"=>10,"IX"=>9,"V"=>5,"IV"=>4,"I"=>1);
     $result="";
     while ($n>0){
-        $n--;
-        $result=$result."I";
-
+        foreach ($map as $key=>$value){
+            if ($n>=$value) {
+                $n = $n - $value;
+                $result=$result.$key;
+                break;
+            }
+        }
     }
-
     return $result;
 }
-
 function test($input, $expected){
     echo "Test : When passing $input returns $expected";
     print_r("<br>Result:".(toRomanNumeral($input)==$expected?"true":"false"));
@@ -20,5 +23,5 @@ test(1,"I");
 test(2,"II");
 test (3,"III");
 test(4,"IV");
-
+test(1333,"MCCCXXXIII");
 
